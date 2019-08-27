04e7b39dd8f61526fdc4742ffd448302b2cdc35e
Roberto Pires <roberto_pires97@sapo.pt>
2019-08-27 01:37:32 +0000

    Updated Packages

---
+++ scripts/build.js
@@ -0,0 +1,32 @@
+var del = require('del');
+const {series} = require('async');
+const {exec} = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+series([
+    (done) => {
+        console.log("Deleting Dist Folder.");
+
+        del([
+            'dist/'
+        ]);
+
+        done();
+    },
+    (done) => {
+        console.log("Compiling Typescript.");
+
+        exec('npm run compile', () => {
+            done();
+        });
+    },
+    (done) => {
+        console.log("Copying Auth & dbData.");
+
+        fs.copyFileSync(path.resolve("auth.json"), path.resolve("dist", "auth.json"));
+        fs.copyFileSync(path.resolve("dbData.json"), path.resolve("dist", "dbData.json"));
+
+        done();
+    }
+]);
